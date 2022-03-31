9eba796b698b951c2870df702892dbcb8979f250
Intiser Rajoan Parash <irparash@gmail.com>
2022-03-31 00:45:55 -0500

    fixed endpoints to localhost

--- src/pages/api-form.js
+++ src/pages/api-form.js
@@ -9,7 +9,7 @@
  * */
 const getTeacherByID = async (id) => {
   try {
-    const response = await fetch(`http://10.76.156.99:8080/api/teacher/${id}`);
+    const response = await fetch(`http://localhost:8080/api/teacher/${id}`);
     return await response.json();
   } catch (err) {
     console.log(err);
@@ -20,7 +20,7 @@ const getTeacherByID = async (id) => {
 const getAllLocations = async () => {
   try {
     const response = await fetch(
-      'http://10.76.156.99:8080/api/location/locations'
+      'http://localhost:8080/api/location/locations'
     );
     return await response.json();
   } catch (err) {
@@ -31,7 +31,7 @@ const getAllLocations = async () => {
 const getShopForm = async (location) => {
   try {
     const response = await fetch(
-      `http://10.76.156.99:8080/api/${location}/form/getShopForm`
+      `http://localhost:8080/api/${location}/form/getShopForm`
     );
     const responseJson = await response.json();
     responseJson.forEach((element) => {
@@ -49,7 +49,7 @@ const submitForm = async (location, items) => {
     items.items = items.items.filter((item) => item.itemCount > 0);
 
     const response = await fetch(
-      `http://10.76.156.99:8080/api/${location}/transaction/submit`,
+      `http://localhost:8080/api/${location}/transaction/submit`,
       {
         method: 'POST',
         headers: {
